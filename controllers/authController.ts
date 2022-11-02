14ecc5b0d5ea42ab683e24b99a2218050927282e
Giuseppe Tarallo <73602435+tarallo-giuseppe@users.noreply.github.com>
2022-11-02 18:14:13 +0100

    Added check on password

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,15 +1,22 @@
 import { UserModel } from "../models"
 import { User } from "../types"
 
+//regex for password validation
+const VALID_PASSWORD: RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/
+
 // This controller is used to handler operations on users
 export function authController(fastify: any, opts: any, next: any) {
   fastify.post("/signup", async (req: any, res: any) => {
     const { email } = req.body
 
+    //if an user inserts a password who does not respect the pattern (one upper case, one special char, min. length 8) rejects the request
+    if (!VALID_PASSWORD.test(req.body.password))
+      return res.code(400).send("Password should have one upper case, one special character and a minimum length of 8")
+
     if (await UserModel.findOne({ email })) return res.code(400).send("Email already used")
 
     // Creates and saves the user
-    const savedUser = await new UserModel(req.body).save()
+    const savedUser: User = await new UserModel(req.body).save()
 
     return savedUser
   })
--- routes/index.ts
+++ routes/index.ts
@@ -1,7 +1,9 @@
 import { userController } from "../controllers/userController"
+import { authController } from "../controllers/authController"
 
 export function routes(fastify: any, opts: any, next: any) {
   fastify.register(userController, { prefix: "/api/users" })
+  fastify.register(authController, { prefix: "/api/auth" })
 
   next()
 }
